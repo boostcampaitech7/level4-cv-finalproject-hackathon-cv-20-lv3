c6083c04f25b880edb0e14624a2b880ac34b2b6f
SuyoungPark11 <suyoung.park1121@gmail.com>
2025-01-16 14:46:21 +0900

    feat: Update config files
    #8

--- audiolm-trainer/dataset.py
+++ audiolm-trainer/dataset.py
@@ -78,7 +78,7 @@ class SALMONNDataset(Dataset):
 
     def __getitem__(self, index):
         ann = self.annotation[index]
-        audio_path = self.prefix + '/' + ann["path"]
+        audio_path = self.prefix + ann["path"]
         try:
             audio, sr = sf.read(audio_path)
         except:
