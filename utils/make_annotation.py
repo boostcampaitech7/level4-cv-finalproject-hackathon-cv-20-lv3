06f9853e4719ab9059525620643acb36c251bf47
nOctaveLay <wjddkdml1124@naver.com>
2025-01-21 12:31:44 +0900

    [✨Feat]: `stage2의 asr,aac`를 `stage1 asr,aac`로 옮겨 `stage1_train.json`을 만듦 Fixes #25

---
+++ utils/make_annotation.py
@@ -0,0 +1,54 @@
+import os
+import json
+from tqdm import tqdm
+
+# 환경 변수 정의
+PATH_PREFIX = 'C:\\Users\\clear\\Documents\\GitHub\\level4-cv-finalproject-hackathon-cv-20-lv3'
+ANNOTATION_PREFIX = os.path.join(PATH_PREFIX, '00_original_annotation')
+STAGE1_TRAIN_PATH = os.path.join(ANNOTATION_PREFIX, 'stage1_train.json')
+STAGE2_TRAIN_PATH = os.path.join(ANNOTATION_PREFIX, 'stage2_train.json')
+
+def read_annotation_and_make_data(path: str):
+    """annotation 파일을 읽고 data로 만들어주는 함수"""
+    base_name = os.path.basename(path)
+    if not base_name.endswith('.json'):
+        print(".json 확장자를 가지고 있지 않습니다.")
+        return None
+    with open(path, 'r') as json_file:
+        return json.load(json_file)
+
+def merge_stage1_and_stage2(stage1_data: dict, stage2_data: dict) -> dict:
+    """stage1_train.json에 stage2_train.json의 asr과 audiocaption_v2 데이터를 병합하는 함수"""
+    
+    # stage2_data에서 'asr'과 'audiocaption_v2'를 찾음
+    extracted_data = []
+    
+    for ann in tqdm(stage2_data['annotation'], desc='extracting asr, audiocaption_v2'):
+        if ann['task'] == 'asr':
+            extracted_data.append(ann)
+        elif ann['task'] == 'audiocaption_v2':
+            ann['task'] = 'audiocaption'
+            extracted_data.append(ann)
+    
+    # stage1_data에 병합 (set을 사용하여 중복 제거)
+    stage1_annotation_set = {json.dumps(item, sort_keys=True) for item in tqdm(stage1_data['annotation'], desc='make stage1_data set')}
+    extracted_data_set = {json.dumps(item, sort_keys=True) for item in extracted_data, desc='make extracted_data set'}
+    
+    # 두 집합의 합집합을 구한 후, 다시 리스트로 변환
+    merged_annotations = list(map(json.loads, stage1_annotation_set.union(extracted_data_set)))
+    
+    stage1_data['annotation'] = merged_annotations
+    return stage1_data
+
+if __name__ == "__main__":
+    stage1_data = read_annotation_and_make_data(STAGE1_TRAIN_PATH)
+    stage2_data = read_annotation_and_make_data(STAGE2_TRAIN_PATH)
+    if not stage1_data or not stage2_data: exit()
+    if stage1_data and stage2_data:
+        new_stage1_data = merge_stage1_and_stage2(stage1_data, stage2_data)
+        new_file_path = STAGE1_TRAIN_PATH.replace('00_original_annotation', 'annotation')
+        
+        with open(new_file_path, 'w') as f:
+            json.dump({"annotation": new_stage1_data}, f, indent=4)
+        
+        print(f"Processed and saved: {new_file_path}")
